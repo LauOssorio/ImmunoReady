12a6fa4354df2af6f8470d8f200eb63b2cf01975
Laura Ossorio <ossoriolaura@gmail.com>
2025-06-02 17:54:47 +0200

    added doc data_cleaning_tool

--- data_cleaning/data_cleaning_tools.py
+++ data_cleaning/data_cleaning_tools.py
@@ -3,6 +3,27 @@
 
 
 def select_columns_and_clean_iedb(data_frame):
+    """
+    Filters and cleans an IEDB dataset by selecting relevant columns and removing
+    observations that are not associated with immunization or disease.
+
+    This function performs the following steps:
+    1. Selects a predefined set of biologically relevant columns related to epitopes,
+       assay conditions, and MHC restrictions.
+    2. Drops duplicate rows to avoid redundant entries.
+    3. Removes rows corresponding to observations from healthy tissues or cases with
+       no immunization, as they are not informative for immunogenicity analysis.
+
+    Parameters:
+    ----------
+    data_frame : pandas.DataFrame
+        The input DataFrame containing IEDB data.
+
+    Returns:
+    -------
+    pandas.DataFrame
+        A cleaned DataFrame containing only relevant columns and filtered observations.
+    """
     list_columns = ['Epitope - Name',
                 'Epitope - Source Organism',
                 'Epitope - Species',
@@ -23,3 +44,6 @@ def select_columns_and_clean_iedb(data_frame):
     (data_frame['1st in vivo Process - Disease'] != "healthy")]
 
     return data_frame
+
+
+# TODO: Check for weird characters in amino acid sequences
