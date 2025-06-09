f160e519d4642c79f6070c3f44f9e3a511c32db1
Laura Ossorio <ossoriolaura@gmail.com>
2025-06-09 12:19:06 +0200

    balance_prepare_training_set() fully developed. releasing first version into the wild

--- immuno_ready/ml_logic/data_preprocessing_pipeline.py
+++ immuno_ready/ml_logic/data_preprocessing_pipeline.py
@@ -8,6 +8,13 @@
 import pandas as pd
 import numpy as np
 from sklearn.utils import resample
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from add_relevant_columns import *
+from peptide_cleaning import *
+from data_cleaning_tools import *
+from aa_dataset import *
+import os
 
 
 
@@ -30,7 +37,6 @@ def slice_pretraining_func(df, number_of_observations):
     safe_df = df[df['peptide_safety'] == 0]
     risky_df = df[df['peptide_safety'] == 1]
 
-
     max_safe = min(len(risky_df), int(number_of_observations))
 
     n_diseases = (safe_df["disease_group"].nunique())
@@ -45,21 +51,54 @@ def slice_pretraining_func(df, number_of_observations):
         for _, group in safe_disease_groups #ignoring the name of the group from GroupBy object
     ])
 
-
-
         # ---- Sample risky peptides ----
     sampled_risky = risky_df.sample(n = max_safe, random_state=42)
 
     # ---- Combine ----
     final_df = pd.concat([balanced_safe, sampled_risky], ignore_index=True)
+
     return final_df
 
 
 
-def prepare_training_set(data_positives, data_negatives, slice_pretraining = True, number_of_observations = 10000):
-    from  immuno_ready.ml_logic.add_relevant_columns import add_relevant_columns
-    from immuno_ready.ml_logic.peptide_cleaning import peptide_cleaning
-    from data_cleaning_tools import select_columns_and_clean_iedb, create_target_features
+def balance_prepare_training_set(data_positives, data_negatives, number_of_observations = 6000):
+
+    """
+    Combines, balances, cleans, and processes positive and negative peptide datasets
+    into a unified training set with target features.
+    If the whole dataset is to be used, set the number of observations to 1000000000.
+    
+    This function:
+    - Cleans and standardizes the input positive and negative datasets.
+    - Merges them into a single DataFrame.
+    - Applies peptide sequence cleaning and filtering.
+    - Generates target features for downstream modeling.
+    - Slices the dataset for pretraining.
+
+    Parameters
+    ----------
+    data_positives : pd.DataFrame
+        DataFrame containing positive samples (e.g. immunogenic peptides).
+    data_negatives : pd.DataFrame
+        DataFrame containing negative samples (e.g. non-immunogenic peptides).
+    number_of_observations : int, optional
+        Number of samples to return if slicing for pretraining. Default is `len_`, which
+        should be defined in the broader scope.
+
+    Returns
+    -------
+    pd.DataFrame
+        A cleaned and processed dataset with target features ready for training or pretraining.
+
+    Notes
+    -----
+    - Uses several utility functions from `immuno_ready.ml_logic` and `data_cleaning_tools`.
+    - Assumes global variables `slice_pretraining` and `slice_pretraining_func` are defined
+      externally to control optional slicing.
+    - Input DataFrames should conform to expected schema for the cleaning and processing functions.
+    """
+
+
 
     data_positives = select_columns_and_clean_iedb(data_positives)
     data_negatives = add_relevant_columns(data_negatives)
@@ -74,8 +113,92 @@ def prepare_training_set(data_positives, data_negatives, slice_pretraining = Tru
 
     dataset_target = create_target_features(full_dataset_clean)
 
-    if slice_pretraining:
-        return slice_pretraining_func(dataset_target, number_of_observations)
+    balanced_training_df = slice_pretraining_func(dataset_target, number_of_observations)
 
-    else:
-        return dataset_target
+    return balanced_training_df
+
+
+
+
+
+parent_path = os.getcwd()
+
+path_1 = os.getcwd()
+parent_path = os.path.dirname(path_1)
+final_path_pca = parent_path + "/data/dataset3_pca.csv"
+
+
+def split_training_set(dataset_target,
+                           validation_size = 0.3,
+                           random_state = 42,
+                           pca_table_path = final_path_pca,
+                           #normalization_method = "MinMaxScaler()"
+                           ):
+
+    """
+    Preprocesses a peptide dataset by splitting it into training and validation sets,
+    and generating feature matrices using PCA-based encoding.
+
+    This function:
+    - Splits the dataset into training and validation sets for both classification
+      (`peptide_safety`) and regression (`target_strength`) targets.
+    - Generates numerical feature matrices for model input using a PCA transformation table.
+    - Pads the peptide sequences to the maximun length
+
+    Parameters
+    ----------
+    dataset_target : pd.DataFrame
+        Input DataFrame containing peptide features and target labels.
+        Must include columns: 'peptide_safety', 'target_strength', and 'disease_group'.
+    validation_size : float, optional (default=0.3)
+        Proportion of the dataset to include in the validation split.
+    random_state : int, optional (default=42)
+        Random seed used to ensure reproducibility of the train/validation split.
+    pca_table_path : str, optional (default="./immuno_ready/data/dataset3_pca.csv")
+        File path to the PCA transformation table used to encode peptide sequences.
+
+    Returns
+    -------
+    y_train_class : pd.Series
+        Training labels for classification (peptide safety).
+    y_val_class : pd.Series
+        Validation labels for classification (peptide safety).
+    y_train_reg : pd.Series
+        Training labels for regression (target strength).
+    y_val_reg : pd.Series
+        Validation labels for regression (target strength).
+    X_train_matrix : np.ndarray or pd.DataFrame
+        Encoded feature matrix for training data.
+    X_val_matrix : np.ndarray or pd.DataFrame
+        Encoded feature matrix for validation data.
+
+    Notes
+    -----
+    This function requires the `generate_matrices_for_dataset` function from the
+    `aa_dataset` module to perform peptide encoding based on the PCA table.
+
+    Examples
+    -----
+    >>> y_train_class, y_val_class, y_train_reg, y_val_reg, X_train_matrix_pad, X_val_matrix_pad = split_training_set(balances_training_set)
+
+    """
+
+    pca_table = pd.read_csv(pca_table_path)
+
+    X = dataset_target.drop(columns=['peptide_safety', 'target_strength', 'disease_group'])
+    y_class = dataset_target['peptide_safety']
+    y_reg = dataset_target['target_strength']
+
+
+    X_train, X_val, y_train_class, y_val_class = train_test_split(X, y_class, test_size = 0.3, random_state = 42)
+    X_train, X_val, y_train_reg, y_val_reg = train_test_split(X, y_reg, test_size = 0.3, random_state = 42)
+
+
+
+    X_train_matrix = generate_matrices_for_dataset(X_train, pca_table)
+    X_val_matrix = generate_matrices_for_dataset(X_val, pca_table)
+
+    X_train_matrix_pad = pad_sequences(X_train_matrix, dtype='float32', padding='post')
+    X_val_matrix_pad = pad_sequences(X_val_matrix, dtype='float32', padding='post')
+
+    return y_train_class, y_val_class, y_train_reg, y_val_reg, X_train_matrix_pad, X_val_matrix_pad
